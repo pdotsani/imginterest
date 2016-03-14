d34e5404b9955c1018c141541e0b95ec69cbdfd1
Patrick San Juan <pat.sanjuan@gmail.com>
2016-03-14 23:49:01 +0000

    login / signin page revamp

--- client/app/app.js
+++ client/app/app.js
@@ -31,7 +31,7 @@ angular.module('imginterestApp', [
       // Intercept 401s and redirect you to login
       responseError: function(response) {
         if(response.status === 401) {
-          $location.path('/login');
+          $location.path('/main');
           // remove any stale tokens
           $cookieStore.remove('token');
           return $q.reject(response);
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,8 +2,15 @@
 
 angular.module('imginterestApp')
   .controller('MainCtrl', function ($scope, $http, $location, Auth, $window) {
+    $scope.onLogin = true;
     $scope.user = {};
     $scope.errors = {};
+    
+    $scope.toggleLoginSignup = function() {
+      $scope.onLogin = !$scope.onLogin;
+      $scope.user = {};
+      $scope.errors = {};
+    };
 
     $scope.login = function(form) {
       $scope.submitted = true;
@@ -19,6 +26,32 @@ angular.module('imginterestApp')
         })
         .catch( function(err) {
           $scope.errors.other = err.message;
+        });
+      }
+    };
+    
+    $scope.register = function(form) {
+      $scope.submitted = true;
+
+      if(form.$valid) {
+        Auth.createUser({
+          name: $scope.user.name,
+          email: $scope.user.email,
+          password: $scope.user.password
+        })
+        .then( function() {
+          // Account created, redirect to home
+          $location.path('/');
+        })
+        .catch( function(err) {
+          err = err.data;
+          $scope.errors = {};
+
+          // Update validity of form fields that match the mongoose errors
+          angular.forEach(err.errors, function(error, field) {
+            form[field].$setValidity('mongoose', false);
+            $scope.errors[field] = error.message;
+          });
         });
       }
     };
